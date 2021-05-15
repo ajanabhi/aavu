cabffbd1b28eeef451d54a9efdee2c82d778b99c
chandu0101 <chandu.csu2010@gmail.com>
2021-05-15 08:44:07 +0530

    update

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -7,6 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Container(
+        child: Text("Aavu :) "),
+      ),
+    );
   }
 }
